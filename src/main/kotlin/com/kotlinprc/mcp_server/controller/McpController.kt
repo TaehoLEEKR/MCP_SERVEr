4db4e12a663f07fc379f9e870e696b58fae3322f
daeho <679748@naver.com>
2025-04-26 21:36:04 +0900

    McpController 폴더

---
+++ src/main/kotlin/com/kotlinprc/mcp_server/controller/McpController.kt
@@ -0,0 +1,16 @@
+package com.kotlinprc.mcp_server.controller
+
+import lombok.RequiredArgsConstructor
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+import reactor.core.publisher.Mono
+
+
+@RestController
+@RequestMapping("/v1")
+@RequiredArgsConstructor
+class McpController {
+//    fun listModeuls(): Mono<Map<String, List<String>>>{
+//
+//    }
+}

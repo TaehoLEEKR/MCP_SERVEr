8ab3542dc7bcc7ada29425155d54dab710f60293
daeho <679748@naver.com>
2025-04-26 21:38:11 +0900

    WebClient config 구성

---
+++ src/main/kotlin/com/kotlinprc/mcp_server/service/OllamaService.kt
@@ -0,0 +1,10 @@
+package com.kotlinprc.mcp_server.service
+
+import lombok.RequiredArgsConstructor
+import org.springframework.stereotype.Service
+
+@Service
+@RequiredArgsConstructor
+class OllamaService {
+
+}

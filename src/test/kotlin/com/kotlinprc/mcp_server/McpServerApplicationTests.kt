c5152861d70407f849ecac4824f3d43143928b63
daeho <679748@naver.com>
2025-04-26 21:28:27 +0900

    first mcp server

---
+++ src/test/kotlin/com/kotlinprc/mcp_server/McpServerApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.kotlinprc.mcp_server
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class McpServerApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
